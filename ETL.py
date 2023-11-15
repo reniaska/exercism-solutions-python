ab9a12ae807492c348bb85af48a7cdc27a653b02
Martyna Reinke <rei.mar@wp.pl>
2023-11-15 15:32:58 +0100

    ETL file filled by code

---
+++ ETL.py
@@ -0,0 +1,9 @@
+#https://exercism.org/tracks/python/exercises/etl
+
+
+def transform(legacy_data):
+    data = {}
+    for key in legacy_data:
+        for letter in legacy_data[key]:
+            data[letter.lower()] = key
+    return data
