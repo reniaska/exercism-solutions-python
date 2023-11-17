a5eec1fed5dac772f9aaae2e02dff4a194693f20
Martyna Reinke <rei.mar@wp.pl>
2023-11-17 17:03:33 +0100

    added missing code

---
+++ Sieve.py
@@ -0,0 +1,11 @@
+#https://exercism.org/tracks/python/exercises/sieve
+
+def primes(limit):
+    numbers = set(range(2, limit + 1))
+    multiplication_list = []
+    for num in range(2, int(limit ** 0.5) + 1):
+        multiplication = 2
+        while num * multiplication <= limit:
+            multiplication_list.append(num * multiplication)
+            multiplication += 1
+    return list(numbers.difference(multiplication_list))
